a71ee4ac5dff656ce8eb1418812d80f48c102fbf
kiwi <buh@fshows.com>
2017-03-09 15:25:26 +0800

    添加配置支持 可指定扩展类的后缀 可指定扩展类添加注解与注解Import 可指定去除前缀

---
+++ src/main/java/xyz/mrwood/mybatis/generator/plugin/common/Constants.java
@@ -0,0 +1,38 @@
+/**
+ * 项目：mybatis-generator-plugins
+ * 包名：xyz.mrwood.mybatis.generator.plugin.common
+ * 功能：
+ * 时间：2017-03-08 13:58
+ * 作者：Mr.Kiwi
+ */
+package xyz.mrwood.mybatis.generator.plugin.common;
+
+/**
+ * 项目：mybatis-generator-plugins
+ * 包名：xyz.mrwood.mybatis.generator.plugin.common
+ * 功能：
+ * 时间：2017-03-08 13:58
+ * 作者：Mr.Kiwi
+ */
+public interface Constants {
+
+    /**
+     * 扩展类添加的注解
+     */
+    String KEY_EXT_CLASS_ADD_ANNO = "extClassAddAnno";
+
+    /**
+     * 扩展类添加的注解类
+     */
+    String KEY_EXT_CLASS_ADD_ANNO_CLASS = "extClassAddAnnoClass";
+
+    /**
+     * 扩展类的后缀
+     */
+    String KEY_EXT_CLASS_SUFFIX = "extClassSuffix";
+
+    /**
+     * 要移除的前缀名称
+     */
+    String KEY_CLASS_REMOVE_PREFIX = "classRemovePrefix";
+}
--- src/main/java/xyz/mrwood/mybatis/generator/plugin/plugins/ExtPlugin.java
+++ src/main/java/xyz/mrwood/mybatis/generator/plugin/plugins/ExtPlugin.java
@@ -17,10 +17,13 @@ import org.mybatis.generator.api.dom.xml.Document;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
 import org.mybatis.generator.codegen.XmlConstants;
+import org.mybatis.generator.config.Context;
+import xyz.mrwood.mybatis.generator.plugin.common.Constants;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * 项目：mybatis-generator-plugin
@@ -31,78 +34,186 @@ import java.util.List;
  */
 public class ExtPlugin extends PluginAdapter {
 
-    public static final String SUFFIX = "Ext";
+    /**
+     * 后缀
+     */
+    private static String suffix;
+    /**
+     * mapper 名称
+     */
+    private static String mapperName;
+    /**
+     * mapper 扩展类名称
+     */
+    private static String mapperExtName;
+    /**
+     * mapper xml名称
+     */
+    private static String mapperXmlName;
+    /**
+     * mapper xml 扩展xml名称
+     */
+    private static String mapperExtXmlName;
+    /**
+     * model 名称
+     */
+    private static String modelName;
+    /**
+     * model ext 名称
+     */
+    private static String modelExtName;
 
+    Logger logger = Logger.getLogger("ExtPlugin");
+
+    /**
+     * 验证是否通过
+     *
+     * @param warnings
+     * @return
+     */
     @Override
     public boolean validate(List<String> warnings) {
         return true;
     }
 
+
+
+    /**
+     * 初始化对像，这里可以修改生成的类
+     *
+     * @param introspectedTable
+     */
+    @Override
+    public void initialized(IntrospectedTable introspectedTable) {
+
+        // 设置后缀
+        setSuffix();
+
+        // 设置model 与 model ext名称
+        modelName = getModel(context, introspectedTable.getBaseRecordType());
+        modelExtName = modelName + suffix;
+
+        // 设置mapper 与 mapper ext 名称
+        mapperName = getMapperName(context, introspectedTable.getMyBatis3JavaMapperType());
+        mapperExtName = mapperName + suffix;
+
+        // 设置mapper xml 与 mapper ext xml 名称
+        mapperXmlName = getMapperXmlName(context, introspectedTable.getMyBatis3XmlMapperFileName());
+        mapperExtXmlName = mapperXmlName.split("\\.")[0] + suffix + ".xml";
+
+        // 修改model类的名称
+        introspectedTable.setBaseRecordType(modelName);
+        // 修改mapper类的名称
+        introspectedTable.setMyBatis3JavaMapperType(mapperName);
+        // 修改mapper xml类的名称
+        introspectedTable.setMyBatis3XmlMapperFileName(mapperXmlName);
+
+        logger.info("初始化："
+            + "\n modleName = " + modelName
+            + "\n modelExtName = " + modelExtName
+            + "\n mapperName = " + mapperName
+            + "\n mapperExtName = " + mapperExtName
+            + "\n mapperXmlName = " + mapperXmlName
+            + "\n mapperExtXmlName = " + mapperExtXmlName
+        );
+
+    }
+
+    /**
+     * 生成额外的javaFiles列表
+     *
+     * @param introspectedTable
+     * @return
+     */
     @Override
     public List<GeneratedJavaFile> contextGenerateAdditionalJavaFiles(IntrospectedTable introspectedTable) {
 
 
         List<GeneratedJavaFile> answer = new ArrayList<>();
 
-        if (!isExistForExtModel(introspectedTable)) {
-            answer.add(createExtModel(introspectedTable));
+        /**
+         * 扩展modle类是否存在
+         */
+        if (!isExistForExtModel()) {
+
+            answer.add(createExtModel());
+
+            logger.info(modelExtName + " -> 生成");
+        }else {
+
+            logger.info(modelExtName + " -> 已存在，不生成");
         }
 
-        if (!isExistForExtMapper(introspectedTable)) {
-            answer.add(createExtMapper(introspectedTable));
+        /**
+         * 扩展的mapper类是否存在
+         */
+        if (!isExistForExtMapper()) {
+
+            answer.add(createExtMapper());
+            logger.info(mapperExtName + " -> 生成");
+
+        }else {
+
+            logger.info(mapperExtName + " -> 已存在，不生成");
         }
         return answer;
     }
 
+    /**
+     * @param document
+     * @param introspectedTable
+     * @return
+     */
     @Override
     public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
-
-        // 获得mapper的全限定名
-        String myBatis3JavaMapperType = introspectedTable.getMyBatis3JavaMapperType();
 
         // 修改namespace为扩展类的
         XmlElement rootElement = document.getRootElement();
         rootElement.getAttributes().clear();
-        rootElement.addAttribute(new Attribute("namespace", myBatis3JavaMapperType + SUFFIX));
+        rootElement.addAttribute(new Attribute("namespace", mapperExtName));
 
         return true;
     }
 
+    /**
+     * 生成额外的xml文件
+     *
+     * @param introspectedTable
+     * @return
+     */
     @Override
     public List<GeneratedXmlFile> contextGenerateAdditionalXmlFiles(IntrospectedTable introspectedTable) {
 
         List<GeneratedXmlFile> answer = new ArrayList<>();
 
-        if (isExistForExtMapperXml(introspectedTable)) {
+        // 判断扩展的xml是否已经存在
+        if (isExistForExtMapperXml()) {
 
+            logger.info(mapperExtXmlName + " -> 已存在，不生成");
             return answer;
+        }else {
+
+            logger.info(mapperExtXmlName + " -> 生成");
         }
 
+        // 创建文档对象
         Document document =
             new Document(XmlConstants.MYBATIS3_MAPPER_PUBLIC_ID, XmlConstants.MYBATIS3_MAPPER_SYSTEM_ID);
 
-        // 获得mapper的全限定名
-        String myBatis3JavaMapperType = introspectedTable.getMyBatis3JavaMapperType();
-
-        XmlElement root = new XmlElement("mapper"); //$NON-NLS-1$
-        String value = myBatis3JavaMapperType + SUFFIX;
-        root.addAttribute(new Attribute("namespace", value));
+        // 创建根节点
+        XmlElement root = new XmlElement("mapper");
+        root.addAttribute(new Attribute("namespace", mapperExtName));
         root.addElement(new TextElement(""));
+
         document.setRootElement(root);
 
 
         String targetPackage = context.getSqlMapGeneratorConfiguration().getTargetPackage();
         String targetProject = context.getSqlMapGeneratorConfiguration().getTargetProject();
 
-        String myBatis3XmlMapperFileName = introspectedTable.getMyBatis3XmlMapperFileName();
-        String[] fileNameArr = myBatis3XmlMapperFileName.split("\\.");
-
-        GeneratedXmlFile gxf = new GeneratedXmlFile(document,
-            fileNameArr[0] + SUFFIX + "." + fileNameArr[1], //$NON-NLS-1$ //$NON-NLS-2$
-            targetPackage, //$NON-NLS-1$
-            targetProject, //$NON-NLS-1$
-            false, context.getXmlFormatter());
-
+        // 生成ext xml文件
+        GeneratedXmlFile gxf =
+            new GeneratedXmlFile(document, mapperExtXmlName, targetPackage, targetProject, false, context.getXmlFormatter());
 
         answer.add(gxf);
 
@@ -112,15 +223,14 @@ public class ExtPlugin extends PluginAdapter {
     /**
      * 创建额外MODEL类
      *
-     * @param introspectedTable
      * @return
      */
-    public GeneratedJavaFile createExtModel(IntrospectedTable introspectedTable) {
+    public GeneratedJavaFile createExtModel() {
 
-        FullyQualifiedJavaType type =
-            new FullyQualifiedJavaType(introspectedTable.getBaseRecordType() + SUFFIX);
+        // 获得model的全限定名
+        FullyQualifiedJavaType type = new FullyQualifiedJavaType(modelExtName);
         TopLevelClass top = new TopLevelClass(type);
-        top.setSuperClass(introspectedTable.getBaseRecordType());
+        top.setSuperClass(getModel(context, modelName));
         top.setVisibility(JavaVisibility.PUBLIC);
 
         GeneratedJavaFile javaFile =
@@ -132,22 +242,36 @@ public class ExtPlugin extends PluginAdapter {
     /**
      * 创建额外mapper接口
      *
-     * @param introspectedTable
      * @return
      */
-    public GeneratedJavaFile createExtMapper(IntrospectedTable introspectedTable) {
+    public GeneratedJavaFile createExtMapper() {
 
         // 从配置文件中获得targetProject属性
         String targetProject = context.getJavaClientGeneratorConfiguration().getTargetProject();
 
-        // 获得mapper的全限定名
-        String myBatis3JavaMapperType = introspectedTable.getMyBatis3JavaMapperType();
-
         // 拼装接口
-        FullyQualifiedJavaType type = new FullyQualifiedJavaType(myBatis3JavaMapperType + SUFFIX);
+        FullyQualifiedJavaType type = new FullyQualifiedJavaType(mapperExtName);
         Interface inter = new Interface(type);
-        inter.addSuperInterface(new FullyQualifiedJavaType(myBatis3JavaMapperType));
+        inter.addSuperInterface(new FullyQualifiedJavaType(mapperName));
         inter.setVisibility(JavaVisibility.PUBLIC);
+
+        // 添加注解 与 注解引入
+        String anno = context.getProperty(Constants.KEY_EXT_CLASS_ADD_ANNO);
+        String annoClass = context.getProperty(Constants.KEY_EXT_CLASS_ADD_ANNO_CLASS);
+
+        System.out.println("anno = " + anno);
+        System.out.println("annoClass = " + annoClass);
+
+        if (anno != null) {
+
+            inter.addAnnotation(anno);
+        }
+
+        if (annoClass != null) {
+
+            inter.addImportedType(new FullyQualifiedJavaType(annoClass));
+        }
+
 
         GeneratedJavaFile javaFile =
             new GeneratedJavaFile(inter, targetProject, new DefaultJavaFormatter());
@@ -158,14 +282,13 @@ public class ExtPlugin extends PluginAdapter {
     /**
      * 判断mapper扩展是否存在
      */
-    public boolean isExistForExtMapper(IntrospectedTable introspectedTable) {
+    public boolean isExistForExtMapper() {
         // 从配置文件中获得targetProject属性
         String targetProject = context.getJavaClientGeneratorConfiguration().getTargetProject();
-        // 获得mapper的全限定名
-        String myBatis3JavaMapperType = introspectedTable.getMyBatis3JavaMapperType();
+
         // Mapper扩展类的文件名
-        String fileName =
-            targetProject + "/" + myBatis3JavaMapperType.replace('.', '/') + SUFFIX + ".java";
+        String fileName = targetProject + "/" + mapperExtName.replace('.', '/') + ".java";
+
         File file = new File(fileName);
         return file.exists();
     }
@@ -173,35 +296,97 @@ public class ExtPlugin extends PluginAdapter {
     /**
      * 判断model扩展是否存在
      */
-    public boolean isExistForExtModel(IntrospectedTable introspectedTable) {
+    public boolean isExistForExtModel() {
         // 从配置文件中获得targetProject属性
         String targetProject = context.getJavaModelGeneratorConfiguration().getTargetProject();
-        // 获得mapper的全限定名
-        String myBatis3JavaMapperType = introspectedTable.getBaseRecordType();
         // Mapper扩展类的文件名
-        String fileName =
-            targetProject + "/" + myBatis3JavaMapperType.replace('.', '/') + SUFFIX + ".java";
-        File file = new File(fileName);
-        return file.exists();
+        String fileName = targetProject + "/" + modelExtName.replace('.', '/') + ".java";
+        return new File(fileName).exists();
     }
 
     /**
      * 判断mapper.xml扩展是否存在
      */
-    public boolean isExistForExtMapperXml(IntrospectedTable introspectedTable) {
+    public boolean isExistForExtMapperXml() {
         // 从配置文件中获得targetProject属性
         String targetProject = context.getSqlMapGeneratorConfiguration().getTargetProject();
         String targetPackage = context.getSqlMapGeneratorConfiguration().getTargetPackage();
 
-        String myBatis3XmlMapperFileName = introspectedTable.getMyBatis3XmlMapperFileName();
-        String[] fileNameArr = myBatis3XmlMapperFileName.split("\\.");
-
         // Mapper扩展类的文件名
-        String fileName = targetProject + "/" + targetPackage + "/" + fileNameArr[0] + SUFFIX + "." + fileNameArr[1];
+        String fileName = targetProject + "/" + targetPackage + "/" + mapperExtXmlName;
 
-        System.err.println(fileName);
+        return new File(fileName).exists();
+    }
 
-        File file = new File(fileName);
-        return file.exists();
+
+    /**
+     * 获得mapper文件名
+     *
+     * @param context
+     * @param mapperFileName
+     * @return
+     */
+    public String getMapperName(Context context, String mapperFileName) {
+
+        // 移除类前缀
+        String classRemovePrefix = context.getProperty(Constants.KEY_CLASS_REMOVE_PREFIX);
+        if (classRemovePrefix != null && !classRemovePrefix.equals("")) {
+
+            mapperFileName = mapperFileName.replace(classRemovePrefix, "");
+        }
+
+        return mapperFileName;
+    }
+
+    /**
+     * 获得mapper xml文件名
+     *
+     * @param context
+     * @param mapperXmlName
+     * @return
+     */
+    public String getMapperXmlName(Context context, String mapperXmlName) {
+
+        // 移除类前缀
+        String classRemovePrefix = context.getProperty(Constants.KEY_CLASS_REMOVE_PREFIX);
+        if (classRemovePrefix != null && !classRemovePrefix.equals("")) {
+
+            mapperXmlName = mapperXmlName.replace(classRemovePrefix, "");
+        }
+
+        return mapperXmlName;
+    }
+
+    /**
+     * 获得model名
+     *
+     * @param context
+     * @param modelName
+     * @return
+     */
+    public String getModel(Context context, String modelName) {
+
+        // 移除类前缀
+        String classRemovePrefix = context.getProperty(Constants.KEY_CLASS_REMOVE_PREFIX);
+        if (classRemovePrefix != null && !classRemovePrefix.equals("")) {
+
+            modelName = modelName.replace(classRemovePrefix, "");
+        }
+
+        return modelName;
+    }
+
+
+    /**
+     * 设置后缀
+     */
+    public void setSuffix() {
+
+        suffix = "Ext";
+        String extSuffix = context.getProperty(Constants.KEY_EXT_CLASS_SUFFIX);
+        if (extSuffix != null && !extSuffix.equals("")) {
+
+            suffix = extSuffix;
+        }
     }
 }
